6b64420c70bea7e3b9a5b15beef286f2e116083f
Hussein Adams <hussein.a.adams@gmail.com>
2018-02-15 09:06:48 -0600

    Added project files

---
+++ app/src/main/java/gymbuddy/project/capstone/gymbuddy/UI/HomePage/HomeActivity.java
@@ -0,0 +1,14 @@
+package gymbuddy.project.capstone.gymbuddy.UI.HomePage;
+
+import gymbuddy.project.capstone.gymbuddy.R;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
