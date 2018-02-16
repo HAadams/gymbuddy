e9963cfebaafa65aa8c7225ddbcd5e716f8eef22
Arham Desktop <arham360@gmail.com>
2018-02-15 23:50:19 -0600

    Added Tinder swipe View. I will install volley for web requests tomorrow and get pictures going in and out. I'll start parsing JSON so it is ready for firebase data. And I will def try and finish the login screen and if need be splash screen by saturday morning.

--- app/src/main/java/gymbuddy/project/capstone/gymbuddy/UI/HomePage/HomeActivity.java
+++ app/src/main/java/gymbuddy/project/capstone/gymbuddy/UI/HomePage/HomeActivity.java
@@ -1,14 +1,63 @@
 package gymbuddy.project.capstone.gymbuddy.UI.HomePage;
 
-import gymbuddy.project.capstone.gymbuddy.R;
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import com.mindorks.placeholderview.SwipeDecor;
+import com.mindorks.placeholderview.SwipePlaceHolderView;
+
+import java.util.List;
+
+import gymbuddy.project.capstone.gymbuddy.Adapters.Profile;
+import gymbuddy.project.capstone.gymbuddy.R;
+import gymbuddy.project.capstone.gymbuddy.Utilities.Utils;
 
 public class HomeActivity extends AppCompatActivity {
+
+    private SwipePlaceHolderView mSwipeView;
+    private Context mContext;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        mSwipeView = (SwipePlaceHolderView)findViewById(R.id.swipeView);
+        mContext = getApplicationContext();
+
+        mSwipeView.getBuilder()
+                .setDisplayViewCount(5)
+                .setSwipeDecor(new SwipeDecor()
+                        .setPaddingTop(20)
+                        .setRelativeScale(0.01f)
+                        .setSwipeInMsgLayoutId(R.layout.tinder_swipe_in_msg_view)
+                        .setSwipeOutMsgLayoutId(R.layout.tinder_swipe_out_msg_view));
+Utils utils = new Utils();
+       List<Profile> arrayList = Utils.loadProfiles(mContext);
+        for(int i = 0 ; i<arrayList.size(); i++){
+            mSwipeView.addView(new TinderCard(mContext, arrayList.get(i), mSwipeView));
+        }
+
+//        for(Profile profile : Utils.loadProfiles(mContext)){
+//            mSwipeView.addView(new TinderCard(mContext, profile, mSwipeView));
+//        }
+
+        findViewById(R.id.rejectBtn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mSwipeView.doSwipe(false);
+            }
+        });
+
+        findViewById(R.id.acceptBtn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mSwipeView.doSwipe(true);
+            }
+        });
     }
-}
+    }
+
