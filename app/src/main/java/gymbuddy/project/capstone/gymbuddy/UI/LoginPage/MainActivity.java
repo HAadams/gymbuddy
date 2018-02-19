b8ad3bab36b527d8c353f8073988fb93f5bb993f
Hussein Adams <hussein.a.adams@gmail.com>
2018-02-19 09:47:02 -0600

    Removed the 3 fragments on main page

--- app/src/main/java/gymbuddy/project/capstone/gymbuddy/UI/LoginPage/MainActivity.java
+++ app/src/main/java/gymbuddy/project/capstone/gymbuddy/UI/LoginPage/MainActivity.java
@@ -2,6 +2,7 @@ package gymbuddy.project.capstone.gymbuddy.UI.LoginPage;
 
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -18,13 +19,22 @@ import android.view.View;
 
 
 import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 import com.firebase.client.Firebase;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FacebookAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 
 import gymbuddy.project.capstone.gymbuddy.Database.FirebaseDatabaseHelper;
 import gymbuddy.project.capstone.gymbuddy.Map.LocationHelper;
 import gymbuddy.project.capstone.gymbuddy.R;
-import gymbuddy.project.capstone.gymbuddy.Adapters.FragmentSelectionPageAdapter;
 import gymbuddy.project.capstone.gymbuddy.UI.HomePage.HomeActivity;
 import gymbuddy.project.capstone.gymbuddy.Utilities.PhotosAPI;
 
@@ -38,7 +48,10 @@ public class MainActivity extends AppCompatActivity {
      * may be best to switch to a
      * {@link android.support.v4.app.FragmentStatePagerAdapter}.
      */
-    private FragmentSelectionPageAdapter fragmentAdapter;
+
+    private CallbackManager mCallbackManager;
+    private final String TAG = "FACEBOOK_AUTH_LOGGER";
+    private FirebaseAuth firebaseAuth;
 
 
     /**
@@ -54,13 +67,6 @@ public class MainActivity extends AppCompatActivity {
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        // Create the adapter that will return a fragment for each of the three
-        // primary sections of the activity.
-        fragmentAdapter = new FragmentSelectionPageAdapter(getSupportFragmentManager());
-
-        // Set up the ViewPager with the sections adapter.
-        mViewPager = (ViewPager) findViewById(R.id.container);
-        mViewPager.setAdapter(fragmentAdapter);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -70,6 +76,33 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        // Initialize Facebook Login button
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        mCallbackManager = CallbackManager.Factory.create();
+        LoginButton loginButton = findViewById(R.id.login_button);
+        loginButton.setReadPermissions("email", "public_profile", "user_birthday", "user_photos");
+        loginButton.registerCallback(mCallbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                Log.d(TAG, "facebook:onSuccess:" + loginResult);
+                handleFacebookAccessToken(loginResult.getAccessToken());
+            }
+
+            @Override
+            public void onCancel() {
+                Log.d(TAG, "facebook:onCancel");
+                // ...
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                Log.d(TAG, "facebook:onError", error);
+                // ...
+            }
+        });
+
     }
 
     @Override
@@ -81,12 +114,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        for (Fragment fragment : getSupportFragmentManager().getFragments()) {
-            if(fragment instanceof LoginFragment)
-                fragment.onActivityResult(requestCode, resultCode, data);
-        }
+        mCallbackManager.onActivityResult(requestCode, resultCode, data);
     }
 
     @Override
@@ -132,6 +160,29 @@ public class MainActivity extends AppCompatActivity {
         Intent accountIntent = new Intent(MainActivity.this, HomeActivity.class);
         startActivity(accountIntent);
         finish();
+    }
+
+    private void handleFacebookAccessToken(final AccessToken token) {
+        Log.d(TAG, "handleFacebookAccessToken:" + token);
+
+        AuthCredential credential = FacebookAuthProvider.getCredential(token.getToken());
+        firebaseAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithCredential:success");
+                            updateUserData(token);
+                            startHomePageActivity();
+
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithCredential:failure", task.getException());
+
+                        }
+                    }
+                });
     }
 
 }
