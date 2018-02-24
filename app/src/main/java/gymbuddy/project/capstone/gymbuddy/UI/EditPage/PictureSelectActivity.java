6688e3666246df96c5ebe51b02844472e6e0f39b
Hussein Adams <hussein.a.adams@gmail.com>
2018-02-24 12:24:37 -0600

    Updated comments

--- app/src/main/java/gymbuddy/project/capstone/gymbuddy/UI/EditPage/PictureSelectActivity.java
+++ app/src/main/java/gymbuddy/project/capstone/gymbuddy/UI/EditPage/PictureSelectActivity.java
@@ -18,50 +18,56 @@ import gymbuddy.project.capstone.gymbuddy.Utilities.PhotosAPI;
 public class PictureSelectActivity extends AppCompatActivity {
     AlbumsSelectAdapter adapter;
     RecyclerView rv;
-    PhotosAPI photosHelper = PhotosAPI.getInstance();
-
+    PhotosAPI photosHelper;
     List<Album> albumList;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_picture_select);
 
+        photosHelper = PhotosAPI.getInstance();
         albumList = FirebaseDatabaseHelper.getInstance().currentUser.albums;
         rv = findViewById(R.id.AlbumSelectRecyclerView);
         rv.setLayoutManager(new LinearLayoutManager(this));
         adapter = new AlbumsSelectAdapter(this, albumList);
         adapter.notifyDataSetChanged();
         rv.setAdapter(adapter);
+        // Clear album data so there won't be any duplicates
         FirebaseDatabaseHelper.getInstance().currentUser.clearAlbums();
+        // Fetch user albums and update the adapter
         getUserAlbums();
 
     }
 
     public void getUserAlbums(){
+        // This executes a thread to fetch user albums from facebook
         photosHelper.fetchUserAlbums();
-        new AlbumsFetchedChecker(adapter).execute();
+        // This waits for the albums to be fetched and notifies the adapter afterwards
+        new CheckAlbumsFetchedAndUpdate(adapter).execute();
     }
 
-    static class AlbumsFetchedChecker extends AsyncTask<Void, Void, Void>{
+    private static class CheckAlbumsFetchedAndUpdate extends AsyncTask<Void, Void, Void>{
         PhotosAPI helper = PhotosAPI.getInstance();
         AlbumsSelectAdapter adapter;
-        AlbumsFetchedChecker(AlbumsSelectAdapter adapter){this.adapter = adapter;}
+        CheckAlbumsFetchedAndUpdate(AlbumsSelectAdapter adapter){this.adapter = adapter;}
 
         @Override
         protected Void doInBackground(Void... voids) {
+            // Wait for albums to be fetched, return on error
             while(!helper.isAlbumsFetchComplete()){
                 if(helper.isErrorOccured()){
                     Log.e(getClass().toString(), "Error occurred while fetching user albums");
                     return null;
                 }
             }
-
             return null;
         }
 
         @Override
         protected void onPostExecute(Void aVoid) {
             super.onPostExecute(aVoid);
+            // Once albums are fetched, notify the adapter to update the list view
             adapter.notifyDataSetChanged();
         }
     }
