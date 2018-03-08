86ff48b1806a2d767f164354ef2288c98bac8899
Arham Akbar <arham360@gmail.com>
2018-03-07 20:09:33 -0600

    Added my todo list items

--- app/src/main/java/gymbuddy/project/capstone/gymbuddy/Todo.java
+++ app/src/main/java/gymbuddy/project/capstone/gymbuddy/Todo.java
@@ -7,13 +7,13 @@ package gymbuddy.project.capstone.gymbuddy;
 //public class Todo {
 // Easy Access Todo List
 //
-//
-//
-//
-//
-//
-//
-//
+//  Arham:
+//  Add mapBox fragment
+//  Add profile fragment
+//  Modify cards to have more information plus add touchAndHold methods
+//  Add message fragment
+//  If performance is not ideal on Nexus 5X, look at activities as alternatives
+//  
 //
 //
 //
