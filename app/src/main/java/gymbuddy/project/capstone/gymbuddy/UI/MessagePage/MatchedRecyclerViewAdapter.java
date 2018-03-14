491246c2d5364131fdbeeeddb2ce579e3059a1e2
Arham Akbar <arham360@gmail.com>
2018-03-14 11:17:49 -0500

    Second recyclerview added.

--- app/src/main/java/gymbuddy/project/capstone/gymbuddy/UI/MessagePage/MatchedRecyclerViewAdapter.java
+++ app/src/main/java/gymbuddy/project/capstone/gymbuddy/UI/MessagePage/MatchedRecyclerViewAdapter.java
@@ -12,15 +12,14 @@ import java.util.List;
 
 import gymbuddy.project.capstone.gymbuddy.Database.User;
 import gymbuddy.project.capstone.gymbuddy.R;
-import gymbuddy.project.capstone.gymbuddy.UI.HomePage.MessengerFragment;
 
 
-public class MessageRecyclerViewAdapter extends RecyclerView.Adapter<MessageRecyclerViewAdapter.ViewHolder> {
+public class MatchedRecyclerViewAdapter extends RecyclerView.Adapter<MatchedRecyclerViewAdapter.ViewHolder> {
 
     private final List<User> mValues;
     private final OnListInteractionListener mListener;
 
-    public MessageRecyclerViewAdapter(List<User> items, OnListInteractionListener listener) {
+    public MatchedRecyclerViewAdapter(List<User> items, OnListInteractionListener listener) {
         mValues = items;
         mListener = listener;
     }
@@ -28,7 +27,7 @@ public class MessageRecyclerViewAdapter extends RecyclerView.Adapter<MessageRecy
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext())
-                .inflate(R.layout.fragment_topic, parent, false);
+                .inflate(R.layout.matched_list, parent, false);
         return new ViewHolder(view);
     }
 
@@ -36,7 +35,6 @@ public class MessageRecyclerViewAdapter extends RecyclerView.Adapter<MessageRecy
     public void onBindViewHolder(final ViewHolder holder, int position) {
         holder.mItem = mValues.get(position);
 
-        holder.mContentView.setText(mValues.get(position).getName());
         holder.simpleDraweeView.setImageURI(mValues.get(position).getPhotoURL());
 
         holder.mView.setOnClickListener(new View.OnClickListener() {
@@ -57,19 +55,13 @@ public class MessageRecyclerViewAdapter extends RecyclerView.Adapter<MessageRecy
     public class ViewHolder extends RecyclerView.ViewHolder {
         public final View mView;
         public final SimpleDraweeView simpleDraweeView;
-        public final TextView mContentView;
         public User mItem;
 
         public ViewHolder(View view) {
             super(view);
             mView = view;
-            simpleDraweeView = view.findViewById(R.id.simpleView);
-            mContentView =  view.findViewById(R.id.content);
+            simpleDraweeView = view.findViewById(R.id.simpleView2);
         }
 
-        @Override
-        public String toString() {
-            return super.toString() + " '" + mContentView.getText() + "'";
-        }
     }
 }
